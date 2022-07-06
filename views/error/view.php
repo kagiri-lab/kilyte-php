cc886a5ea6a0c7383645bfb7de4141b033317997
Kagiri <kagiri@kagiri.com>
2022-07-06 18:18:51 +0300

    Error render update

--- views/error/view.php
+++ views/error/view.php
@@ -1,11 +1,69 @@
-<?php $this->title = $exception->getCode() . " - Error" ?>
+<?php $this->title = $error['code'] . " - Error" ?>
 
 
 <div class="container">
     <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
-        <h1><?php echo $exception->getCode() ?></h1>
-        <br />
-        <h2><?php echo $exception->getMessage() ?></h2>
-        <a class="btn" href="/">Back to home</a>
+        <div class="container">
+            <div class="row">
+                <h1><?= $error['code'] ?></h1>
+                <br />
+                <p><?= $error['message'] ?></p>
+            </div>
+            <?php
+            $debug = "false";
+            if (isset($_ENV["DEBUG"])) {
+                $debug = $_ENV["DEBUG"];
+            }
+            if (strtolower($debug) == 'true') {
+                echo "<hr />";
+                foreach ($error['trace'] as $rs) {
+                    // print_r($rs);
+                    $file = $rs['file'];
+                    $line = $rs['line'];
+                    $func = $rs['function'];
+                    $class = $rs['class'];
+                    $args = $rs['args'];
+            ?>
+                    <div class="row">
+                        <div class="col-12">
+                            <p><strong>File : </strong> <?= $file ?></p>
+                        </div>
+                        <div class="col-12">
+                            <p><strong>Line : </strong> <?= $line ?></p>
+                        </div>
+                        <div class="col-12">
+                            <p><strong>Function : </strong> <?= $func ?></p>
+                        </div>
+                        <div class="col-12">
+                            <p><strong>Class : </strong> <?= $class ?></p>
+                        </div>
+                        <div class="col-12">
+                            <p><strong>Args </strong></p>
+                            <?php print_r($args); ?>
+                        </div>
+
+                        <div class="col-12">
+                            <hr />
+                        </div>
+
+                    </div>
+
+            <?php
+                }
+            }
+            ?>
+
+
+
+        </div>
+
+
+        <div class="col-12">
+            <a class="btn btn-primary" href="/">Back to home</a>
+        </div>
+
+
+
+
     </section>
 </div>
